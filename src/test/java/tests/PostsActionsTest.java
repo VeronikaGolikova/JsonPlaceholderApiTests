7cdb09a692b8ca023bcd510d97a91ef642ff8346
Veronika Golikova <VGolikova@alfabank.ru>
2024-04-23 15:32:59 +0300

    Fix allure-steps naming

--- src/test/java/tests/PostsActionsTest.java
+++ src/test/java/tests/PostsActionsTest.java
@@ -1,8 +1,13 @@
 package tests;
 
+import io.qameta.allure.Epic;
+import io.qameta.allure.Owner;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
 import model.PostsReguestModel;
 import model.PostsResponseModel;
 import model.PutReguestModel;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static io.qameta.allure.Allure.step;
@@ -14,6 +19,9 @@ import static specs.GeneralSpec.loggingResponseSpec;
 import static specs.GeneralSpec.requestSpec;
 import static specs.GeneralSpec.responseSpec200OkWithLogging;
 
+@Owner("golikovavi")
+@Severity(SeverityLevel.CRITICAL)
+@Epic("API-тесты на сервис для работы с постами пользователей")
 public class PostsActionsTest extends TestBase{
 
     String titleTextUser1 = "sunt aut facere repellat provident occaecati excepturi optio reprehenderit";
@@ -22,8 +30,9 @@ public class PostsActionsTest extends TestBase{
     String newBody = "Любимый рецепт, рекомендую. Перейдите по ссылке http://somelink.com";
 
     @Test
+    @DisplayName("Отправка GET-запроса для получения информации по посту")
     void getPostInformation() {
-        PostsResponseModel response = step("Make request", ()->
+        PostsResponseModel response = step("Отправка get-request", ()->
                 given()
                         .spec(requestSpec)
                         .when()
@@ -33,9 +42,8 @@ public class PostsActionsTest extends TestBase{
                         .body(matchesJsonSchemaInClasspath("schemas/postSchema.json"))
                         .extract().as(PostsResponseModel.class));
 
-        step("Check response", ()->
-                assertAll ("Проверка полей пользователя в ответе",
-                           () -> assertEquals(1, response.getUserId()),
+        step("Проверка полей пользователя в ответе", ()->
+                assertAll (() -> assertEquals(1, response.getUserId()),
                            () -> assertEquals(1, response.getId()),
                            () -> assertEquals(titleTextUser1, response.getTitle()),
                            () -> assertEquals(bodyTextUser1, response.getBody())
@@ -44,12 +52,13 @@ public class PostsActionsTest extends TestBase{
     }
 
     @Test
+    @DisplayName("Отправка POST-запроса для создания нового поста")
     void createNewPost() {
         PostsReguestModel requestBody = new PostsReguestModel();
         requestBody.setTitle(newTitle);
         requestBody.setBody(newBody);
         requestBody.setUserId(1);
-        PostsResponseModel response = step("Make request", ()->
+        PostsResponseModel response = step("Отправка post-request", ()->
                 given()
                         .spec(requestSpec)
                         .body(requestBody)
@@ -61,9 +70,8 @@ public class PostsActionsTest extends TestBase{
                         .body(matchesJsonSchemaInClasspath("schemas/postSchema.json"))
                         .extract().as(PostsResponseModel.class));
 
-        step("Check response", ()->
-                assertAll ("Проверка полей пользователя в ответе",
-                           () -> assertEquals(101, response.getId()),
+        step("Проверка полей пользователя в ответе", ()->
+                assertAll (() -> assertEquals(101, response.getId()),
                            () -> assertEquals(1, response.getUserId()),
                            () -> assertEquals(newTitle, response.getTitle()),
                            () -> assertEquals(newBody, response.getBody())
@@ -72,6 +80,7 @@ public class PostsActionsTest extends TestBase{
     }
 
     @Test
+    @DisplayName("Отправка PUT-запроса для редактирования поста")
     void updatePost() {
         PutReguestModel requestBody = new PutReguestModel();
         requestBody.setTitle(newTitle);
@@ -79,7 +88,7 @@ public class PostsActionsTest extends TestBase{
         requestBody.setUserId(1);
         requestBody.setId(1);
         int id = requestBody.getId();
-        PostsResponseModel response = step("Make request", ()->
+        PostsResponseModel response = step("Отправка put-request", ()->
                 given()
                         .spec(requestSpec)
                         .when()
@@ -90,9 +99,8 @@ public class PostsActionsTest extends TestBase{
                         .body(matchesJsonSchemaInClasspath("schemas/postSchema.json"))
                         .extract().as(PostsResponseModel.class));
 
-        step("Check response", ()->
-                assertAll ("Проверка полей пользователя в ответе",
-                           () -> assertEquals(1, response.getId()),
+        step("Проверка полей пользователя в ответе", ()->
+                assertAll (() -> assertEquals(1, response.getId()),
                            () -> assertEquals(1, response.getUserId()),
                            () -> assertEquals(newTitle, response.getTitle()),
                            () -> assertEquals(newBody, response.getBody())
@@ -101,13 +109,14 @@ public class PostsActionsTest extends TestBase{
     }
 
     @Test
+    @DisplayName("Отправка PATCH-запроса для обновления информации в посте")
     void patchResource() {
         PutReguestModel requestBody = new PutReguestModel();
         requestBody.setTitle(newTitle);
         requestBody.setBody(newBody);
         requestBody.setId(1);
         requestBody.setUserId(1);
-        PostsResponseModel response = step("Make request", ()->
+        PostsResponseModel response = step("Отправка patch-request", ()->
                 given()
                         .spec(requestSpec)
                         .when()
@@ -118,9 +127,8 @@ public class PostsActionsTest extends TestBase{
                         .body(matchesJsonSchemaInClasspath("schemas/postSchema.json"))
                         .extract().as(PostsResponseModel.class));
 
-        step("Check response", ()->
-                assertAll ("Проверка полей пользователя в ответе",
-                           () -> assertEquals(1, response.getId()),
+        step("Проверка полей пользователя в ответе", ()->
+                assertAll (() -> assertEquals(1, response.getId()),
                            () -> assertEquals(1, response.getUserId()),
                            () -> assertEquals(newTitle, response.getTitle()),
                            () -> assertEquals(newBody, response.getBody())
@@ -129,8 +137,9 @@ public class PostsActionsTest extends TestBase{
     }
 
     @Test
+    @DisplayName("Отправка DELETE-запроса для удаления поста")
     void deleteResource() {
-        step("Make request", ()->
+        step("Отправка delete-request", ()->
                 given()
                         .spec(requestSpec)
                         .when()
